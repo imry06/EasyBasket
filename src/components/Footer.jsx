e8a2c9128c1f5357fcc139a906d858d77a297858
Rajdev Yadav <rajdevyadavofficial@gmail.com>
2026-07-03 23:58:35 -0400

    footer updated.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import "../css/footer.css";
+
 
 const Footer = () => {
   return (
     <div className="footer">
-        <div className="logo">akka-anna</div>
-        <p>&copy; 2026 Akka Anna. All rights reserved.</p>
+        <h3 className="logo">akka-anna</h3>
+        <ul className="social-icons">
+            <li><a href="https://www.facebook.com/akkaanna" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-facebook"></i></a></li>
+            <li><a href="https://www.instagram.com/akkaanna" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-instagram"></i></a></li>
+            <li><a href="https://www.twitter.com/akkaanna" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-twitter"></i></a></li>
+            <li><a href="https://www.linkedin.com/company/akkaanna" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-linkedin"></i></a></li>
+        </ul>
+
+        <hr />
+        <p>FSSAI No : 1234567890</p>
     </div>
   )
 }
