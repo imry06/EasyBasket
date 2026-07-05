afddceaa517223c88e8f847fa931e6d2ee05b19f
Rajdev Yadav <rajdevyadavofficial@gmail.com>
2026-07-05 15:14:11 -0400

    product-card added.

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,48 @@
+import { useDispatch, useSelector } from "react-redux";
+import {
+  addToCart,
+  increaseQty,
+  decreaseQty,
+} from "../features/cart/cartSlice";
+
+export default function ProductCard({ product }) {
+  const dispatch = useDispatch();
+
+  const cartItem = useSelector((state) =>
+    state.cart.items.find((item) => item.id === product.id)
+  );
+
+  return (
+    <div className="card">
+      <img src={product.image} alt={product.name} />
+
+      <h3>{product.name}</h3>
+
+      <p>{product.unit}</p>
+
+      <h2>₹{product.price}</h2>
+
+      {!cartItem ? (
+        <button onClick={() => dispatch(addToCart(product))}>
+          Add to Cart
+        </button>
+      ) : (
+        <div className="qty-box">
+          <button
+            onClick={() => dispatch(decreaseQty(product.id))}
+          >
+            -
+          </button>
+
+          <span>{cartItem.quantity}</span>
+
+          <button
+            onClick={() => dispatch(increaseQty(product.id))}
+          >
+            +
+          </button>
+        </div>
+      )}
+    </div>
+  );
+}
