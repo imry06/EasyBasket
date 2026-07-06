a42b1479b8d2995a6547d90351d83ad70bb79a4b
Rajdev Yadav <rajdevyadavofficial@gmail.com>
2026-07-06 02:56:51 -0400

    product-list component added.

---
+++ src/components/ProductList.jsx
@@ -0,0 +1,46 @@
+import ProductItem from "../components/ProductItem";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "../css/product-list.css";
+
+const ProductList = ({ category, products }) => {
+  return (
+    <div className="product-list">
+      <h2>{category}</h2>
+
+      <Swiper
+        modules={[Navigation]}
+        navigation
+        spaceBetween={20}
+        breakpoints={{
+          320: {
+            slidesPerView: 1,
+          },
+          576: {
+            slidesPerView: 2,
+          },
+          768: {
+            slidesPerView: 3,
+          },
+          1024: {
+            slidesPerView: 4,
+          },
+          1440: {
+            slidesPerView: 5,
+          },
+        }}
+      >
+        {products.map((product) => (
+          <SwiperSlide key={product.id}>
+            <ProductItem product={product} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
+
+export default ProductList;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,17 @@
 import AkkaAnnaCarousel from "../components/AkkaAnnaCarousel";
 import Products from "../pages/Products"
-import products from "../data/products";
+
+import ProductList from "../components/ProductList";
+
+// data 
+import dalAndPulses from "../data/dalAndPulses";
 
 export default function Home() {
   return (
     <div className="container">
       <Products />
+
+      <ProductList category={"Dal & Pulses"} products={dalAndPulses} />
     </div>
   );
 }
