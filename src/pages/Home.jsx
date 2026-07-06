276d67e8ef48a09a09217003f5114c42a7a4f298
Rajdev Yadav <rajdevyadavofficial@gmail.com>
2026-07-06 03:20:03 -0400

    rice atta grains data displayed successfully.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,11 +4,14 @@ import ProductList from "../components/ProductList";
 
 // data
 import dalAndPulses from "../data/dalAndPulses";
+import riceAttaAndGrains from "../data/riceAttaAndGrains";
+
 
 export default function Home() {
   return (
     <div className="container">
       <ProductList category={"Dal & Pulses"} products={dalAndPulses} />
+      <ProductList category={"Rice, Atta & Grains"} products={riceAttaAndGrains} />
     </div>
   );
 }
