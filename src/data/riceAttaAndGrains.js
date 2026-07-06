aec6e3d0fba485b740d290c03b704032fd4e4add
Rajdev Yadav <rajdevyadavofficial@gmail.com>
2026-07-06 12:04:42 -0400

    rice atta and grains id updated.

--- src/data/riceAttaAndGrains.js
+++ src/data/riceAttaAndGrains.js
@@ -1,6 +1,6 @@
 const riceAttaAndGrains = [
   {
-    id: 1,
+    id: "RAAG1",
     name: "Sona Masoori Rice",
     image: "https://picsum.photos/seed/sonamasoori/400/400",
     measurementUnit: "kg",
@@ -10,7 +10,7 @@ const riceAttaAndGrains = [
     off: "7%"
   },
   {
-    id: 2,
+    id: "RAAG2",
     name: "Basmati Rice Premium",
     image: "https://picsum.photos/seed/basmatirice/400/400",
     measurementUnit: "kg",
@@ -20,7 +20,7 @@ const riceAttaAndGrains = [
     off: "6%"
   },
   {
-    id: 3,
+    id: "RAAG3",
     name: "Brown Rice",
     image: "https://picsum.photos/seed/brownrice/400/400",
     measurementUnit: "kg",
@@ -30,7 +30,7 @@ const riceAttaAndGrains = [
     off: "10%"
   },
   {
-    id: 4,
+    id: "RAAG4",
     name: "Kolam Rice",
     image: "https://picsum.photos/seed/kolamrice/400/400",
     measurementUnit: "kg",
@@ -40,7 +40,7 @@ const riceAttaAndGrains = [
     off: "7%"
   },
   {
-    id: 5,
+    id: "RAAG5",
     name: "Steam Rice",
     image: "https://picsum.photos/seed/steamrice/400/400",
     measurementUnit: "kg",
@@ -50,7 +50,7 @@ const riceAttaAndGrains = [
     off: "6%"
   },
   {
-    id: 6,
+    id: "RAAG6",
     name: "Whole Wheat Atta",
     image: "https://picsum.photos/seed/wheatatta/400/400",
     measurementUnit: "kg",
@@ -60,7 +60,7 @@ const riceAttaAndGrains = [
     off: "7%"
   },
   {
-    id: 7,
+    id: "RAAG7",
     name: "Multigrain Atta",
     image: "https://picsum.photos/seed/multigrainatta/400/400",
     measurementUnit: "kg",
@@ -70,7 +70,7 @@ const riceAttaAndGrains = [
     off: "8%"
   },
   {
-    id: 8,
+    id: "RAAG8",
     name: "Maida",
     image: "https://picsum.photos/seed/maida/400/400",
     measurementUnit: "kg",
@@ -80,7 +80,7 @@ const riceAttaAndGrains = [
     off: "11%"
   },
   {
-    id: 9,
+    id: "RAAG9",
     name: "Suji (Rava)",
     image: "https://picsum.photos/seed/suji/400/400",
     measurementUnit: "kg",
@@ -90,7 +90,7 @@ const riceAttaAndGrains = [
     off: "12%"
   },
   {
-    id: 10,
+    id: "RAAG10",
     name: "Besan",
     image: "https://picsum.photos/seed/besan/400/400",
     measurementUnit: "kg",
@@ -100,7 +100,7 @@ const riceAttaAndGrains = [
     off: "8%"
   },
   {
-    id: 11,
+    id: "RAAG11",
     name: "Jowar Flour",
     image: "https://picsum.photos/seed/jowarflour/400/400",
     measurementUnit: "kg",
@@ -110,7 +110,7 @@ const riceAttaAndGrains = [
     off: "7%"
   },
   {
-    id: 12,
+    id: "RAAG12",
     name: "Bajra Flour",
     image: "https://picsum.photos/seed/bajraflour/400/400",
     measurementUnit: "kg",
@@ -120,7 +120,7 @@ const riceAttaAndGrains = [
     off: "8%"
   },
   {
-    id: 13,
+    id: "RAAG13",
     name: "Ragi Flour",
     image: "https://picsum.photos/seed/ragiflour/400/400",
     measurementUnit: "kg",
@@ -130,7 +130,7 @@ const riceAttaAndGrains = [
     off: "9%"
   },
   {
-    id: 14,
+    id: "RAAG14",
     name: "Poha Thick",
     image: "https://picsum.photos/seed/poha/400/400",
     measurementUnit: "kg",
@@ -140,7 +140,7 @@ const riceAttaAndGrains = [
     off: "8%"
   },
   {
-    id: 15,
+    id: "RAAG15",
     name: "Daliya (Broken Wheat)",
     image: "https://picsum.photos/seed/daliya/400/400",
     measurementUnit: "kg",
