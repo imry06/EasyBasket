b0715a1882abcfd1c6a49a06fbd1954bb542df2f
Rajdev Yadav <rajdevyadavofficial@gmail.com>
2026-07-06 02:20:21 -0400

    footer updated.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,16 +5,9 @@ import "../css/footer.css";
 const Footer = () => {
   return (
     <div className="footer">
-        <h3 className="logo">EasyBasket</h3>
-        <ul className="social-icons">
-            <li><a href="https://www.facebook.com/akkaanna" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-facebook"></i></a></li>
-            <li><a href="https://www.instagram.com/akkaanna" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-instagram"></i></a></li>
-            <li><a href="https://www.twitter.com/akkaanna" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-twitter"></i></a></li>
-            <li><a href="https://www.linkedin.com/company/akkaanna" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-linkedin"></i></a></li>
-        </ul>
-
-        <hr />
-        <p>FSSAI No : 1234567890</p>
+        <h2 className="logo">akka-anna Basket</h2>
+       <h3>"From Our Basket to Your Home."</h3>
+        {/* <p>FSSAI No : 1234567890</p> */}
     </div>
   )
 }
