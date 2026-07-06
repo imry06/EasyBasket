9c1898b815a5c2d7c9759f69c6110b9f331f9c8a
Rajdev Yadav <rajdevyadavofficial@gmail.com>
2026-07-06 03:15:57 -0400

    rice-atta-grains data added.

---
+++ src/data/riceAttaAndGrains.js
@@ -0,0 +1,154 @@
+const riceAttaAndGrains = [
+  {
+    id: 1,
+    name: "Sona Masoori Rice",
+    image: "https://picsum.photos/seed/sonamasoori/400/400",
+    measurementUnit: "kg",
+    size: "5 kg",
+    mrp: 420,
+    sellingPrice: 389,
+    off: "7%"
+  },
+  {
+    id: 2,
+    name: "Basmati Rice Premium",
+    image: "https://picsum.photos/seed/basmatirice/400/400",
+    measurementUnit: "kg",
+    size: "5 kg",
+    mrp: 720,
+    sellingPrice: 679,
+    off: "6%"
+  },
+  {
+    id: 3,
+    name: "Brown Rice",
+    image: "https://picsum.photos/seed/brownrice/400/400",
+    measurementUnit: "kg",
+    size: "1 kg",
+    mrp: 165,
+    sellingPrice: 149,
+    off: "10%"
+  },
+  {
+    id: 4,
+    name: "Kolam Rice",
+    image: "https://picsum.photos/seed/kolamrice/400/400",
+    measurementUnit: "kg",
+    size: "5 kg",
+    mrp: 450,
+    sellingPrice: 419,
+    off: "7%"
+  },
+  {
+    id: 5,
+    name: "Steam Rice",
+    image: "https://picsum.photos/seed/steamrice/400/400",
+    measurementUnit: "kg",
+    size: "10 kg",
+    mrp: 760,
+    sellingPrice: 715,
+    off: "6%"
+  },
+  {
+    id: 6,
+    name: "Whole Wheat Atta",
+    image: "https://picsum.photos/seed/wheatatta/400/400",
+    measurementUnit: "kg",
+    size: "5 kg",
+    mrp: 310,
+    sellingPrice: 289,
+    off: "7%"
+  },
+  {
+    id: 7,
+    name: "Multigrain Atta",
+    image: "https://picsum.photos/seed/multigrainatta/400/400",
+    measurementUnit: "kg",
+    size: "5 kg",
+    mrp: 395,
+    sellingPrice: 365,
+    off: "8%"
+  },
+  {
+    id: 8,
+    name: "Maida",
+    image: "https://picsum.photos/seed/maida/400/400",
+    measurementUnit: "kg",
+    size: "1 kg",
+    mrp: 70,
+    sellingPrice: 62,
+    off: "11%"
+  },
+  {
+    id: 9,
+    name: "Suji (Rava)",
+    image: "https://picsum.photos/seed/suji/400/400",
+    measurementUnit: "kg",
+    size: "1 kg",
+    mrp: 68,
+    sellingPrice: 60,
+    off: "12%"
+  },
+  {
+    id: 10,
+    name: "Besan",
+    image: "https://picsum.photos/seed/besan/400/400",
+    measurementUnit: "kg",
+    size: "1 kg",
+    mrp: 125,
+    sellingPrice: 115,
+    off: "8%"
+  },
+  {
+    id: 11,
+    name: "Jowar Flour",
+    image: "https://picsum.photos/seed/jowarflour/400/400",
+    measurementUnit: "kg",
+    size: "1 kg",
+    mrp: 95,
+    sellingPrice: 88,
+    off: "7%"
+  },
+  {
+    id: 12,
+    name: "Bajra Flour",
+    image: "https://picsum.photos/seed/bajraflour/400/400",
+    measurementUnit: "kg",
+    size: "1 kg",
+    mrp: 90,
+    sellingPrice: 83,
+    off: "8%"
+  },
+  {
+    id: 13,
+    name: "Ragi Flour",
+    image: "https://picsum.photos/seed/ragiflour/400/400",
+    measurementUnit: "kg",
+    size: "1 kg",
+    mrp: 105,
+    sellingPrice: 96,
+    off: "9%"
+  },
+  {
+    id: 14,
+    name: "Poha Thick",
+    image: "https://picsum.photos/seed/poha/400/400",
+    measurementUnit: "kg",
+    size: "1 kg",
+    mrp: 85,
+    sellingPrice: 78,
+    off: "8%"
+  },
+  {
+    id: 15,
+    name: "Daliya (Broken Wheat)",
+    image: "https://picsum.photos/seed/daliya/400/400",
+    measurementUnit: "kg",
+    size: "1 kg",
+    mrp: 78,
+    sellingPrice: 72,
+    off: "8%"
+  }
+];
+
+export default riceAttaAndGrains;
