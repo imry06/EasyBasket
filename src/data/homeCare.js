87207cdc6da81c6822df637c2795ab62c9daf163
Rajdev Yadav <rajdevyadavofficial@gmail.com>
2026-07-06 04:04:34 -0400

    home care items added.

---
+++ src/data/homeCare.js
@@ -0,0 +1,154 @@
+const homeCare = [
+  {
+    id: 1,
+    name: "Detergent Powder",
+    image: "https://picsum.photos/seed/detergentpowder/400/400",
+    measurementUnit: "kg",
+    size: "1 kg",
+    mrp: 180,
+    sellingPrice: 165,
+    off: "8%"
+  },
+  {
+    id: 2,
+    name: "Detergent Liquid",
+    image: "https://picsum.photos/seed/detergentliquid/400/400",
+    measurementUnit: "L",
+    size: "1 L",
+    mrp: 240,
+    sellingPrice: 219,
+    off: "9%"
+  },
+  {
+    id: 3,
+    name: "Dishwash Bar",
+    image: "https://picsum.photos/seed/dishwashbar/400/400",
+    measurementUnit: "pcs",
+    size: "250 g",
+    mrp: 35,
+    sellingPrice: 32,
+    off: "9%"
+  },
+  {
+    id: 4,
+    name: "Dishwash Liquid",
+    image: "https://picsum.photos/seed/dishwashliquid/400/400",
+    measurementUnit: "ml",
+    size: "500 ml",
+    mrp: 120,
+    sellingPrice: 109,
+    off: "9%"
+  },
+  {
+    id: 5,
+    name: "Floor Cleaner",
+    image: "https://picsum.photos/seed/floorcleaner/400/400",
+    measurementUnit: "L",
+    size: "1 L",
+    mrp: 210,
+    sellingPrice: 195,
+    off: "7%"
+  },
+  {
+    id: 6,
+    name: "Toilet Cleaner",
+    image: "https://picsum.photos/seed/toiletcleaner/400/400",
+    measurementUnit: "ml",
+    size: "500 ml",
+    mrp: 115,
+    sellingPrice: 105,
+    off: "9%"
+  },
+  {
+    id: 7,
+    name: "Glass Cleaner",
+    image: "https://picsum.photos/seed/glasscleaner/400/400",
+    measurementUnit: "ml",
+    size: "500 ml",
+    mrp: 140,
+    sellingPrice: 129,
+    off: "8%"
+  },
+  {
+    id: 8,
+    name: "Surface Cleaner",
+    image: "https://picsum.photos/seed/surfacecleaner/400/400",
+    measurementUnit: "ml",
+    size: "500 ml",
+    mrp: 145,
+    sellingPrice: 132,
+    off: "9%"
+  },
+  {
+    id: 9,
+    name: "Fabric Softener",
+    image: "https://picsum.photos/seed/fabricsoftener/400/400",
+    measurementUnit: "ml",
+    size: "860 ml",
+    mrp: 240,
+    sellingPrice: 220,
+    off: "8%"
+  },
+  {
+    id: 10,
+    name: "Air Freshener",
+    image: "https://picsum.photos/seed/airfreshener/400/400",
+    measurementUnit: "ml",
+    size: "300 ml",
+    mrp: 220,
+    sellingPrice: 199,
+    off: "10%"
+  },
+  {
+    id: 11,
+    name: "Garbage Bags",
+    image: "https://picsum.photos/seed/garbagebags/400/400",
+    measurementUnit: "pcs",
+    size: "30 pcs",
+    mrp: 90,
+    sellingPrice: 82,
+    off: "9%"
+  },
+  {
+    id: 12,
+    name: "Aluminium Foil",
+    image: "https://picsum.photos/seed/aluminiumfoil/400/400",
+    measurementUnit: "m",
+    size: "18 m",
+    mrp: 130,
+    sellingPrice: 119,
+    off: "8%"
+  },
+  {
+    id: 13,
+    name: "Cling Film",
+    image: "https://picsum.photos/seed/clingfilm/400/400",
+    measurementUnit: "m",
+    size: "20 m",
+    mrp: 110,
+    sellingPrice: 99,
+    off: "10%"
+  },
+  {
+    id: 14,
+    name: "Mosquito Coil",
+    image: "https://picsum.photos/seed/mosquitocoil/400/400",
+    measurementUnit: "pcs",
+    size: "10 pcs",
+    mrp: 55,
+    sellingPrice: 49,
+    off: "11%"
+  },
+  {
+    id: 15,
+    name: "Mosquito Liquid Vaporizer Refill",
+    image: "https://picsum.photos/seed/mosquitorefill/400/400",
+    measurementUnit: "ml",
+    size: "45 ml",
+    mrp: 95,
+    sellingPrice: 86,
+    off: "9%"
+  }
+];
+
+export default homeCare;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,18 +10,26 @@ import spicesAndMasala from "../data/spicesAndMasala";
 import dairyAndBakery from "../data/dairyAndBakery";
 import teaCoffeeAndBeverages from "../data/teaCoffeeAndBeverages";
 import personalCare from "../data/personalCare";
+import homeCare from "../data/homeCare";
 
 export default function Home() {
   return (
     <div className="container">
       <AkkaAnnaCarousel />
       <ProductList category={"Dal & Pulses"} products={dalAndPulses} />
-      <ProductList category={"Rice, Atta & Grains"} products={riceAttaAndGrains} />
+      <ProductList
+        category={"Rice, Atta & Grains"}
+        products={riceAttaAndGrains}
+      />
       <ProductList category={"Oil & Ghee"} products={oilAndGhee} />
       <ProductList category={"Spices & Masala"} products={spicesAndMasala} />
       <ProductList category={"Dairy & Bakery"} products={dairyAndBakery} />
-      <ProductList category={"Tea, Coffee & Beverages"} products={teaCoffeeAndBeverages} />
-<ProductList category={"Personal Care"} products={personalCare} />
+      <ProductList
+        category={"Tea, Coffee & Beverages"}
+        products={teaCoffeeAndBeverages}
+      />
+      <ProductList category={"Personal Care"} products={personalCare} />
+      <ProductList category={"Home Care"} products={homeCare} />
     </div>
   );
 }
