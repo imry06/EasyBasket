95a250a3b93c8bbadc981ae52675890c45873bab
Rajdev Yadav <rajdevyadavofficial@gmail.com>
2026-07-06 01:57:48 -0400

    productCard component removed.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,11 @@
 import AkkaAnnaCarousel from "../components/AkkaAnnaCarousel";
-import ProductCard from "../components/ProductCard";
+import Products from "../pages/Products"
 import products from "../data/products";
 
 export default function Home() {
   return (
     <div className="container">
-      {/* <AkkaAnnaCarousel /> */}
-
-      <h2>All Products</h2>
-      <div className="products-grid">
-        {products.map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
-      </div>
+      <Products />
     </div>
   );
 }
