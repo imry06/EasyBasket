f74d586203ee0e24eb40915b6525dcef4bb24903
Rajdev Yadav <rajdevyadavofficial@gmail.com>
2026-07-06 03:23:22 -0400

    oil and ghee added successfully.

---
+++ src/data/oilAndGhee.js
@@ -0,0 +1,154 @@
+const oilAndGhee = [
+  {
+    id: 1,
+    name: "Sunflower Oil",
+    image: "https://picsum.photos/seed/sunfloweroil/400/400",
+    measurementUnit: "L",
+    size: "1 L",
+    mrp: 185,
+    sellingPrice: 172,
+    off: "7%"
+  },
+  {
+    id: 2,
+    name: "Sunflower Oil",
+    image: "https://picsum.photos/seed/sunfloweroil5l/400/400",
+    measurementUnit: "L",
+    size: "5 L",
+    mrp: 895,
+    sellingPrice: 845,
+    off: "6%"
+  },
+  {
+    id: 3,
+    name: "Groundnut Oil",
+    image: "https://picsum.photos/seed/groundnutoil/400/400",
+    measurementUnit: "L",
+    size: "1 L",
+    mrp: 235,
+    sellingPrice: 219,
+    off: "7%"
+  },
+  {
+    id: 4,
+    name: "Mustard Oil",
+    image: "https://picsum.photos/seed/mustardoil/400/400",
+    measurementUnit: "L",
+    size: "1 L",
+    mrp: 220,
+    sellingPrice: 205,
+    off: "7%"
+  },
+  {
+    id: 5,
+    name: "Rice Bran Oil",
+    image: "https://picsum.photos/seed/ricebranoil/400/400",
+    measurementUnit: "L",
+    size: "1 L",
+    mrp: 180,
+    sellingPrice: 168,
+    off: "7%"
+  },
+  {
+    id: 6,
+    name: "Soybean Oil",
+    image: "https://picsum.photos/seed/soybeanoil/400/400",
+    measurementUnit: "L",
+    size: "1 L",
+    mrp: 175,
+    sellingPrice: 162,
+    off: "7%"
+  },
+  {
+    id: 7,
+    name: "Cottonseed Oil",
+    image: "https://picsum.photos/seed/cottonseedoil/400/400",
+    measurementUnit: "L",
+    size: "1 L",
+    mrp: 178,
+    sellingPrice: 165,
+    off: "7%"
+  },
+  {
+    id: 8,
+    name: "Coconut Oil",
+    image: "https://picsum.photos/seed/coconutoil/400/400",
+    measurementUnit: "ml",
+    size: "500 ml",
+    mrp: 240,
+    sellingPrice: 225,
+    off: "6%"
+  },
+  {
+    id: 9,
+    name: "Olive Oil",
+    image: "https://picsum.photos/seed/oliveoil/400/400",
+    measurementUnit: "ml",
+    size: "500 ml",
+    mrp: 620,
+    sellingPrice: 579,
+    off: "7%"
+  },
+  {
+    id: 10,
+    name: "Sesame Oil",
+    image: "https://picsum.photos/seed/sesameoil/400/400",
+    measurementUnit: "ml",
+    size: "500 ml",
+    mrp: 290,
+    sellingPrice: 269,
+    off: "7%"
+  },
+  {
+    id: 11,
+    name: "Cow Ghee",
+    image: "https://picsum.photos/seed/cowghee/400/400",
+    measurementUnit: "ml",
+    size: "500 ml",
+    mrp: 390,
+    sellingPrice: 359,
+    off: "8%"
+  },
+  {
+    id: 12,
+    name: "Cow Ghee",
+    image: "https://picsum.photos/seed/cowghee1l/400/400",
+    measurementUnit: "L",
+    size: "1 L",
+    mrp: 760,
+    sellingPrice: 709,
+    off: "7%"
+  },
+  {
+    id: 13,
+    name: "Buffalo Ghee",
+    image: "https://picsum.photos/seed/buffaloghee/400/400",
+    measurementUnit: "ml",
+    size: "500 ml",
+    mrp: 420,
+    sellingPrice: 389,
+    off: "7%"
+  },
+  {
+    id: 14,
+    name: "Vanaspati Ghee",
+    image: "https://picsum.photos/seed/vanaspatighee/400/400",
+    measurementUnit: "kg",
+    size: "1 kg",
+    mrp: 230,
+    sellingPrice: 215,
+    off: "7%"
+  },
+  {
+    id: 15,
+    name: "Cold Pressed Groundnut Oil",
+    image: "https://picsum.photos/seed/coldpressedoil/400/400",
+    measurementUnit: "L",
+    size: "1 L",
+    mrp: 340,
+    sellingPrice: 315,
+    off: "7%"
+  }
+];
+
+export default oilAndGhee;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,19 @@
-// import AkkaAnnaCarousel from "../components/AkkaAnnaCarousel";
+import AkkaAnnaCarousel from "../components/AkkaAnnaCarousel";
 
 import ProductList from "../components/ProductList";
 
 // data
 import dalAndPulses from "../data/dalAndPulses";
 import riceAttaAndGrains from "../data/riceAttaAndGrains";
-
+import oilAndGhee from "../data/oilAndGhee";
 
 export default function Home() {
   return (
     <div className="container">
+      <AkkaAnnaCarousel />
       <ProductList category={"Dal & Pulses"} products={dalAndPulses} />
       <ProductList category={"Rice, Atta & Grains"} products={riceAttaAndGrains} />
+      <ProductList category={"Oil & Ghee"} products={oilAndGhee} />
     </div>
   );
 }
