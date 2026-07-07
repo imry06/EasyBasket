cecd0bef2a84dc2ac8e120355ac5e370f0cff803
Rajdev Yadav <rajdevyadavofficial@gmail.com>
2026-07-07 17:37:58 -0400

    profile form functionality updated.

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import {configureStore} from '@reduxjs/toolkit';
 import cartReducer from "../features/cart/cartSlice";
+import profileReducer from "../features/profile/profileSlice"
 
 export const store = configureStore({
     reducer: {
         cart: cartReducer,
+        profile:profileReducer,
     },
 });
