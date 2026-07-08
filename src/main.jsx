2aa83a103d491b685677ba35e0ec8583c0d6c2f7
Rajdev Yadav <rajdevyadavofficial@gmail.com>
2026-07-08 01:12:11 -0400

    scroll-to-top component added.

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,13 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./app/store.js"; 
 import { ToastContainer } from "react-toastify";
+import ScrollToTop from "./components/ScrollToTop.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
       <BrowserRouter>
+      <ScrollToTop/>
         <App />
         <ToastContainer theme="colored" position="top-right" autoClose={3000}   />
       </BrowserRouter>
